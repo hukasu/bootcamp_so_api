a87036b164f16d09fce8e20cf1a2d888027b8b0c
Lucas Franca <lucasfnf@gmail.com>
2023-01-11 17:48:54 +0900

    End of stage 1

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -23,19 +23,17 @@ pub async fn delete_question(
     todo!()
 }
 
-// ---- CRUD for Answers ----
+#[post("/answer", data = "<answer>")]
+pub async fn create_answer(answer: Json<Answer>) -> Json<AnswerDetail> {
+    todo!()
+}
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//       
-//       hint: this function should look very similar to the create_question function above
+#[get("/answer", data = "<question_uuid>")]
+pub async fn read_answers(question_uuid: Json<QuestionId>) -> Json<AnswerDetail> {
+    todo!()
+}
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//       
-//       hint: this function should look very similar to the read_questions function above
-
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//       
-//       hint: this function should look very similar to the delete_question function above
+#[delete("/answer", data = "<answer_uuid>")]
+pub async fn delete_answer(answer_uuid: Json<AnswerId>) {
+    todo!()
+}
--- src/models.rs
+++ src/models.rs
@@ -2,36 +2,38 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Question {
-    // TODO: add a public `title` field of type String
-    // TODO: add a public `description` field of type String
+    pub title: String,
+    pub description: String
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct QuestionDetail {
-    // TODO: add a public `question_uuid` field of type String
-    // TODO: add a public `title` field of type String
-    // TODO: add a public `description` field of type String
-    // TODO: add a public `created_at` field of type String
+    pub question_uuid: String,
+    pub title: String,
+    pub description: String,
+    pub created_at: String
 }
 
-// TODO: create a struct called `QuestionId`
-//       derive the following traits: Serialize, Deserialize
-//       add a public `question_uuid` field of type String
+#[derive(Serialize, Deserialize)]
+pub struct QuestionId {
+    pub question_uuid: String
+}
 
-// ----------
+#[derive(Serialize, Deserialize)]
+pub struct Answer {
+    pub question_uuid: String,
+    pub content: String
+}
 
-// TODO: create a struct called `Answer`
-//       derive the following traits: Serialize, Deserialize
-//       add a public `question_uuid` field of type String
-//       add a public `content` field of type String
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct AnswerDetail {
+    pub answer_uuid: String,
+    pub question_uuid: String,
+    pub content: String,
+    pub created_at: String
+}
 
-// TODO: create a struct called `AnswerDetail`
-//       derive the following traits: Serialize, Deserialize, Debug, Clone, PartialEq
-//       add a public `answer_uuid` field of type String
-//       add a public `question_uuid` field of type String
-//       add a public `content` field of type String
-//       add a public `created_at` field of type String
-
-// TODO: create a struct called `AnswerId`
-//       derive the following traits: Serialize, Deserialize
-//       add a public `answer_uuid` field of type String
+#[derive(Serialize, Deserialize)]
+pub struct AnswerId {
+    answer_uuid: String
+}
